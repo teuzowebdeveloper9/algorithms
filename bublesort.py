2fcc936333c2035d3583d6091fe2de43d2c993c6
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-03-13 12:47:04 -0300

    bublesort.py

---
+++ bublesort.py
@@ -0,0 +1,9 @@
+#bublesort glub glub glub
+def bublesort(array):
+  item = len(array)
+  for j in range(item-1)
+  for i in range(item-1):
+    if array[i] > array[i + 1]:
+      #mandrake LOL
+      array[i],array[i + 1] = array[i + 1] , array[i]
+  
